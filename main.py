4920e48ed74a7657833541ed765d665f57a509e0
Tiago <tiagotbw@gmail.com>
2025-12-19 10:11:37 -0300

    refactor: move count_words function to stats.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+from stats import count_words
+
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-
-
-def count_words(text):
-    return len(text.split())
 
 
 def main():
