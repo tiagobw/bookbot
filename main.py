032bc7fe2b22636e8f550328c9ef0b2bbfb3cdfa
Tiago <tiagotbw@gmail.com>
2025-12-20 14:32:15 -0300

    feat: add system arguments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, count_chars, generate_sort_chars, print_sorted_chars
 
 
@@ -7,12 +8,16 @@ def get_book_text(filepath):
 
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    if (len(sys.argv)) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    book_text = get_book_text(f"./{book_path}")
     num_words = count_words(book_text)
     chars_dict = count_chars(book_text)
     sorted_chars = generate_sort_chars(chars_dict)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
