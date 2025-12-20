28a3fe854489d0a02de32f449596ef7d57a7356c
Tiago <tiagotbw@gmail.com>
2025-12-20 14:10:43 -0300

    feat: print a report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_chars
+from stats import count_words, count_chars, generate_sort_chars, print_sorted_chars
 
 
 def get_book_text(filepath):
@@ -10,8 +10,14 @@ def main():
     book_text = get_book_text("./books/frankenstein.txt")
     num_words = count_words(book_text)
     chars_dict = count_chars(book_text)
+    sorted_chars = generate_sort_chars(chars_dict)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(chars_dict)
+    print("--------- Character Count -------")
+    print_sorted_chars(sorted_chars)
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,26 @@ def count_chars(text):
             result[lower_char] = 0
         result[lower_char] += 1
     return result
+
+
+def get_num(sorted_chars):
+    return sorted_chars["num"]
+
+
+def generate_sort_chars(chars_count):
+    result = []
+    for key, value in chars_count.items():
+        item = {}
+        item["char"] = key
+        item["num"] = value
+        result.append(item)
+    result.sort(reverse=True, key=get_num)
+    return result
+
+
+def print_sorted_chars(sorted_chars):
+    for item in sorted_chars:
+        char = item["char"]
+        num = item["num"]
+        if char.isalpha():
+            print(f"{char}: {num}")
